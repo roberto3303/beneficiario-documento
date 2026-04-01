2a4b12bd0f646c173a651a8342ae1fde7fe3b1bd
Roberto Pedro <robertopedro100@gmail.com>
2026-04-01 01:16:51 -0300

    inicia projeto

---
+++ src/main/java/br/com/wakanda/beneficiario_documento/BeneficiarioDocumentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.wakanda.beneficiario_documento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiarioDocumentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiarioDocumentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wakanda/beneficiario_documento/BeneficiarioDocumentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wakanda.beneficiario_documento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficiarioDocumentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
