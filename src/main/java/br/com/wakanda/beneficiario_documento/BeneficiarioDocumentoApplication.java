abafc619784b55ddf5bb5c20f790482540fe3fb5
Roberto Pedro <robertopedro100@gmail.com>
2026-04-01 16:42:47 -0300

    implementa handler ao projeto, configura yaml

--- src/main/java/br/com/wakanda/beneficiario_documento/BeneficiarioDocumentoApplication.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/BeneficiarioDocumentoApplication.java
@@ -2,6 +2,7 @@ package br.com.wakanda.beneficiario_documento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BeneficiarioDocumentoApplication {
