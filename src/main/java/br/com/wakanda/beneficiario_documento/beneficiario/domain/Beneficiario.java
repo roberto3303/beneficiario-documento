ef3563659fe9db239f378a76927a3da255ed61e9
Roberto Pedro <robertopedro100@gmail.com>
2026-04-02 17:49:56 -0300

    implementa camada de dominio

---
+++ src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/domain/Beneficiario.java
@@ -0,0 +1,42 @@
+package br.com.wakanda.beneficiario_documento.beneficiario.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Beneficiario {
+    @Id
+    private UUID idBeneficiario;
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    private String telefone;
+    @NotNull
+    private LocalDate dataNascimento;
+
+
+    private LocalDateTime dataInclusao;
+    private LocalDateTime dataAtualizacao;
+
+    public Beneficiario(UUID idBeneficiario, String nomeCompleto,
+                        String telefone, LocalDate dataNascimento,
+                        LocalDateTime dataInclusao, LocalDateTime dataAtualizacao) {
+        this.idBeneficiario = idBeneficiario;
+        this.nomeCompleto = nomeCompleto;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.dataInclusao = LocalDateTime.now();
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
