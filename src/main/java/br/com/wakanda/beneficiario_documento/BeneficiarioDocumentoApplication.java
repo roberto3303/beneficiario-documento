aa2eb78feb32c181c5e53fa0ac3615ad960f5796
Roberto Pedro <robertopedro100@gmail.com>
2026-04-02 14:06:27 -0300

    Teste de api

--- src/main/java/br/com/wakanda/beneficiario_documento/BeneficiarioDocumentoApplication.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/BeneficiarioDocumentoApplication.java
@@ -3,10 +3,18 @@ package br.com.wakanda.beneficiario_documento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class BeneficiarioDocumentoApplication {
 
+    @GetMapping
+    public String getHomeTeste(){
+        return "Beneficiario Documento - API";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BeneficiarioDocumentoApplication.class, args);
 	}
