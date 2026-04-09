53445c54d95570991f50e15288cc5c496ca2c188
Roberto Pedro <robertopedro100@gmail.com>
2026-04-09 17:52:36 -0300

    implementa repository

--- src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/application/service/BeneficiarioApplicationService.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/application/service/BeneficiarioApplicationService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Log4j2
 @RequiredArgsConstructor
 public class BeneficiarioApplicationService implements BeneficiarioService {
+
     private final BeneficiarioRepository beneficiarioRepository;
 
     @Override
