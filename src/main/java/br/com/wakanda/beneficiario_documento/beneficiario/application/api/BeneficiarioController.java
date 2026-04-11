571aebae8198fc46cc9dd6983db56f223231c8c7
Roberto Pedro <robertopedro100@gmail.com>
2026-04-11 16:54:49 -0300

    implementa controller + fluxo service

--- src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/application/api/BeneficiarioController.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/application/api/BeneficiarioController.java
@@ -35,7 +35,8 @@ public class BeneficiarioController implements BeneficiarioAPI {
     public BeneficiarioDetalhadoResponse getBeneficiarioAtravesId(UUID idBeneficiario) {
         log.info("[inicia] BeneficiarioController - getBeneficiarioAtravesId");
         log.info("[IdBeneficiario] {}", idBeneficiario);
+        BeneficiarioDetalhadoResponse beneficiarioDetalhado = beneficiarioService.buscaBeneficiarioAtravesId(idBeneficiario);
         log.info("[finaliza] BeneficiarioController - getBeneficiarioAtravesId");
-        return null;
+        return beneficiarioDetalhado;
     }
 }
--- src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/application/service/BeneficiarioApplicationService.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/application/service/BeneficiarioApplicationService.java
@@ -1,5 +1,6 @@
 package br.com.wakanda.beneficiario_documento.beneficiario.application.service;
 
+import br.com.wakanda.beneficiario_documento.beneficiario.application.api.BeneficiarioDetalhadoResponse;
 import br.com.wakanda.beneficiario_documento.beneficiario.application.api.BeneficiarioListResponse;
 import br.com.wakanda.beneficiario_documento.beneficiario.application.api.BeneficiarioResponse;
 import br.com.wakanda.beneficiario_documento.beneficiario.application.api.BeneficiarioRequest;
@@ -10,6 +11,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Log4j2
@@ -33,5 +35,12 @@ public class BeneficiarioApplicationService implements BeneficiarioService {
         List<Beneficiario> beneficiarios = beneficiarioRepository.buscaTodosBeneficiarios();
         log.info("[finaliza] BeneficiarioApplicationService - buscaTodosBeneficiarios");
         return BeneficiarioListResponse.converte(beneficiarios);
+    }
+
+    @Override
+    public BeneficiarioDetalhadoResponse buscaBeneficiarioAtravesId(UUID idBeneficiario) {
+        log.info("[inicia] BeneficiarioApplicationService - buscaBeneficiarioAtravesId");
+        log.info("[finaliza] BeneficiarioApplicationService - buscaBeneficiarioAtravesId");
+        return null;
     }
 }
