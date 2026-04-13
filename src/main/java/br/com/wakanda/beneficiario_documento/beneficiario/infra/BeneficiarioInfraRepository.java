7d127b30410542d04344350c1f5a0018f42eef08
Roberto Pedro <robertopedro100@gmail.com>
2026-04-13 12:17:33 -0300

    corrige HttpStatusCode

--- src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/infra/BeneficiarioInfraRepository.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/beneficiario/infra/BeneficiarioInfraRepository.java
@@ -39,7 +39,7 @@ public class BeneficiarioInfraRepository implements BeneficiarioRepository {
     public Beneficiario buscaBeneficiarioAtravesId(UUID idBeneficiario) {
         log.info("[inicia] BeneficiarioInfraRepository - buscaBeneficiarioAtravesId");
         Beneficiario beneficiario = beneficiarioSpringDataJpaRepository.findById(idBeneficiario)
-                .orElseThrow(()-> APIException.build(HttpStatus.BAD_REQUEST,"Beneficiario Nao encontrado"));
+                .orElseThrow(()-> APIException.build(HttpStatus.NOT_FOUND,"Beneficiario Nao encontrado"));
         log.info("[finaliza] BeneficiarioInfraRepository - buscaBeneficiarioAtravesId");
         return beneficiario;
     }
