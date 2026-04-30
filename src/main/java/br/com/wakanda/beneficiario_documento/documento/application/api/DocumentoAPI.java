2065a2b5b8877c64c46dcd6eb4bb600e103c7d32
Roberto Pedro <robertopedro100@gmail.com>
2026-04-30 13:02:29 -0300

    implementa contrato API e fluxo controller

--- src/main/java/br/com/wakanda/beneficiario_documento/documento/application/api/DocumentoAPI.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/documento/application/api/DocumentoAPI.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,5 +17,7 @@ public interface DocumentoAPI {
     DocumentoResponse postDocumento(@PathVariable UUID idBeneficiario,
                                     @Valid @RequestBody DocumentoRequest documentoRequest);
 
-
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    List<DocumentoListResponse> getDocumentosDoBeneficiario(@PathVariable UUID idBeneficiario);
 }
--- src/main/java/br/com/wakanda/beneficiario_documento/documento/application/api/DocumentoController.java
+++ src/main/java/br/com/wakanda/beneficiario_documento/documento/application/api/DocumentoController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,5 +22,12 @@ public class DocumentoController implements DocumentoAPI {
         DocumentoResponse documento = documentoService.criaDocumento(idBeneficiario, documentoRequest);
         log.info("[finaliza] DocumentoController - postDocumento");
         return documento;
+    }
+
+    @Override
+    public List<DocumentoListResponse> getDocumentosDoBeneficiario(UUID idBeneficiario) {
+        log.info("[inicia] DocumentoController - getDocumentosDoBeneficiario");
+        log.info("[finaliza] DocumentoController - getDocumentosDoBeneficiario");
+        return List.of();
     }
 }
